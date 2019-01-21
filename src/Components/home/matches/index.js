ed0c5fd420f83b45746eed5cec15437189f65610
Nicholas Wease <skynet60@outlook.com>
2019-01-21 12:03:41 +0100

    Continued work on matches component.

--- src/Components/home/matches/index.js
+++ src/Components/home/matches/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import {Tag} from '../../ui/miscellaneous';
 
 const MatchesHome = () => {
     return (
         <div className='home_matches_wrapper'>
             <div className="container">
-
+                <Tag bck='#0e1731'
+                     size='50px'
+                     color='#fff'
+                >
+                    Matches
+                </Tag>
             </div>
         </div>
     );
