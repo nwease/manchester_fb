47879e5e538e8a71ef08f760fe85cb04a40b6669
Nicholas Wease <skynet60@outlook.com>
2019-01-23 11:22:34 +0100

    Began working on Promotions.

---
+++ src/Components/home/promotion/Enroll.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Zoom from 'react-reveal/Zoom';
+import Jersey from '../../../Resources/images/jersey.jpg';
+
+const PromotionAnimation = () => {
+    return (
+        <div className='promotion_animation'>
+            <div className="left">
+                <div>
+                    <span>
+                        Win a
+                    </span>
+                    <span>
+                        Jersey
+                    </span>
+                </div>
+            </div>
+
+            <div className="right">
+
+            </div>
+        </div>
+    );
+};
+
+export default PromotionAnimation;
