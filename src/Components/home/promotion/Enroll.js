beebd648845ae51ec82eed18339f266864271616
Nicholas Wease <skynet60@outlook.com>
2019-01-23 11:45:27 +0100

    Continued working on Promotions.

--- src/Components/home/promotion/Enroll.js
+++ src/Components/home/promotion/Enroll.js
@@ -6,18 +6,25 @@ const PromotionAnimation = () => {
     return (
         <div className='promotion_animation'>
             <div className="left">
-                <div>
-                    <span>
-                        Win a
-                    </span>
-                    <span>
-                        Jersey
-                    </span>
-                </div>
+                <Zoom>
+                    <div>
+                        <span>
+                            Win a
+                        </span>
+
+                        <span>
+                            Jersey
+                        </span>
+                    </div>
+                </Zoom>
             </div>
 
             <div className="right">
+                <Zoom>
+                    <div style={{background: `url(${Jersey}) no-repeat`}}>
 
+                    </div>
+                </Zoom>
             </div>
         </div>
     );
