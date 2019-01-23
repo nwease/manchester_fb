cf7399910e0d846c50e69d26a18c0f5d0252df94
Nicholas Wease <skynet60@outlook.com>
2019-01-23 10:41:29 +0100

    Continued work on the animated cards.

--- src/Components/home/meetPlayers/Cards.js
+++ src/Components/home/meetPlayers/Cards.js
@@ -6,31 +6,57 @@ import Otamendi from '../../../Resources/images/players/Otamendi.png';
 class HomeCards extends Component {
 
     state = {
-        show: this.props.show,
         cards: [
             {
                 bottom: 90,
                 left: 300
             },
             {
-                bottom: 90,
-                left: 300
+                bottom: 60,
+                left: 200
             },
             {
-                bottom: 90,
-                left: 300
+                bottom: 30,
+                left: 100
             },
             {
-                bottom: 90,
-                left: 300
+                bottom: 0,
+                left: 0
             }
         ]
     };
 
     showAnimatedCards = () => (
-        <div>
-            CARDS
-        </div>
+        this.state.cards.map((card, i) => (
+            <Animate
+                key={i}
+                show={this.props.show}
+                start={{
+                    left: 0,
+                    bottom: 0
+                }}
+                enter={{
+                    left: [card.left],
+                    bottom: [card.bottom],
+                    timing: {duration: 500, ease: easePolyOut}
+                }}
+            >
+
+                {({left, bottom}) => {
+                    return (
+                        <div
+                            style={{
+                                position: 'absolute',
+                                left,
+                                bottom
+                            }}
+                        >
+                            CARDS
+                        </div>
+                    )
+                }}
+            </Animate>
+        ))
     );
 
     render() {
