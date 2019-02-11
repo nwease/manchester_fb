efb3ad16b522c83d34ce825ab32750014fb56d21
Nicholas Wease <skynet60@outlook.com>
2019-02-11 11:40:40 +0100

    Began initial setup of Dashboard Component.

---
+++ src/Components/admin/Dashboard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Dashboard = () => {
+    return (
+        <div className='user_dashboard'>
+            <div>
+                DASHBOARD
+            </div>
+        </div>
+    );
+};
+
+export default Dashboard;
--- src/Routes.js
+++ src/Routes.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import Layout from "./Hoc/Layout";
-import {Switch, Route} from "react-router-dom";
+import Layout from './Hoc/Layout';
+import {Switch, Route} from 'react-router-dom';
 import Home from './Components/home';
-import SignIn from "./Components/signin";
+import SignIn from './Components/signin';
+import Dashboard from './Components/admin/Dashboard';
 
 const Routes = (props) => {
     return (
         <Layout>
             <Switch>
+                <Route exact component={Dashboard} path='/dashboard' />
                 <Route exact component={SignIn} path='/sign_in' />
                 <Route exact component={Home} path='/' />
             </Switch>
