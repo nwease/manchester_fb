ca5c4da7087d46b43d82d5d0c776c1999a99df83
Nicholas Wease <skynet60@outlook.com>
2019-02-21 12:09:23 +0100

    Worked on setting up matches table.

--- src/Routes.js
+++ src/Routes.js
@@ -2,17 +2,20 @@ import React from 'react';
 import Layout from './Hoc/Layout';
 import {Switch, Route} from 'react-router-dom';
 import PrivateRoute from './Components/RouteAuth/privateRoutes';
+import PublicRoute from './Components/RouteAuth/publicRoutes';
 import Home from './Components/home';
 import SignIn from './Components/signin';
 import Dashboard from './Components/admin/Dashboard';
+import Matches from './Components/admin/matches';
 
 const Routes = (props) => {
     return (
         <Layout>
             <Switch>
+                <PrivateRoute {...props} path='/admin_matches' exact component={Matches} />
                 <PrivateRoute {...props} path='/dashboard' exact component={Dashboard} />
-                <Route exact component={SignIn} path='/sign_in' />
-                <Route exact component={Home} path='/' />
+                <PublicRoute {...props} restricted={true} path='/sign_in' exact component={SignIn} />
+                <PublicRoute {...props} restricted={false} path='/' exact component={Home} />
             </Switch>
         </Layout>
     );
