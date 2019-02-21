a76dbdaf70dcc7cce6a11dc479d1f3930d26215d
Nicholas Wease <skynet60@outlook.com>
2019-02-21 10:15:54 +0100

    Worked on authentication.

--- src/Components/admin/nav/AdminNav.js
+++ src/Components/admin/nav/AdminNav.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import ListItem from '@material-ui/core/ListItem';
+import {firebase}from '../../../firebase';
 
 const AdminNav = () => {
 
@@ -23,17 +24,36 @@ const AdminNav = () => {
         }
     ];
 
-    const renderItems = () => (
-        Links.map(Link => (
-            <Link to={Link.linkTo} key={Link.title}>
+    const style = {
+        color: '#fff',
+        fontWeight: '300',
+        borderBottom: '1px solid #353535'
+    };
 
+    const renderItems = () => (
+        Links.map(link => (
+            <Link to={link.linkTo} key={link.title}>
+                <ListItem button style={style}>
+                    {link.title}
+                </ListItem>
             </Link>
         ))
     );
 
+    const logoutHandler = () => {
+        firebase.auth().signOut().then(() => {
+            console.log('Logged Out')
+        }, (error) => {
+            console.log('Error')
+        })
+    };
+
     return (
         <div>
             {renderItems()}
+            <ListItem button style={style} onClick={() => logoutHandler()}>
+                Log Out
+            </ListItem>
         </div>
     );
 };
