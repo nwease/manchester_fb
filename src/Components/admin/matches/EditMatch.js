33b3f4c4b90b96826ceed4e060c879306b51bb10
Nicholas Wease <skynet60@outlook.com>
2019-02-22 12:35:09 +0100

    Messed around with team away results function.

--- src/Components/admin/matches/EditMatch.js
+++ src/Components/admin/matches/EditMatch.js
@@ -34,7 +34,53 @@ class EditMatch extends Component {
                     label: 'Choose a local team',
                     name: 'choose_team',
                     type: 'select',
-                    options: []
+                    options: [{ key: 'Yes', value: 'Yes', }, { key: 'No', value: 'No' }]
+                },
+                validation: {
+                    required: true
+                },
+                valid: false,
+                validationMessage: '',
+                showLabel: false
+            },
+            resultLocal: {
+                element: 'input',
+                value: '',
+                config: {
+                    label: 'Result Local',
+                    name: 'result_local',
+                    type: 'text'
+                },
+                validation: {
+                    required: true
+                },
+                valid: false,
+                validationMessage: '',
+                showLabel: false
+            },
+            away: {
+                element: 'select',
+                value: '',
+                config: {
+                    label: 'Choose a local team',
+                    name: 'choose_team',
+                    type: 'select',
+                    options: [{ key: 'Yes', value: 'Yes', }, { key: 'No', value: 'No' }]
+                },
+                validation: {
+                    required: true
+                },
+                valid: false,
+                validationMessage: '',
+                showLabel: false
+            },
+            resultAway: {
+                element: 'input',
+                value: '',
+                config: {
+                    label: 'Result Away',
+                    name: 'result_away',
+                    type: 'text'
                 },
                 validation: {
                     required: true
@@ -62,11 +108,53 @@ class EditMatch extends Component {
                                 change={(element) => this.updateForm(element)}
                             />
 
-                            <FormField
-                                id={'local'}
-                                formData={this.state.formData.local}
-                                change={(element) => this.updateForm(element)}
-                            />
+                            <div className='select_team_layout'>
+                                <div className='label_inputs'>
+                                    Local
+                                </div>
+
+                                <div className='wrapper'>
+                                    <div className='left'>
+                                        <FormField
+                                            id={'local'}
+                                            formData={this.state.formData.local}
+                                            change={(element) => this.updateForm(element)}
+                                        />
+                                    </div>
+
+                                    <div>
+                                        <FormField
+                                            id={'resultLocal'}
+                                            formData={this.state.formData.resultLocal}
+                                            change={(element) => this.updateForm(element)}
+                                        />
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className='select_team_layout'>
+                                <div className='label_inputs'>
+                                    Away
+                                </div>
+
+                                <div className='wrapper'>
+                                    <div className='left'>
+                                        <FormField
+                                            id={'away'}
+                                            formData={this.state.formData.away}
+                                            change={(element) => this.updateForm(element)}
+                                        />
+                                    </div>
+
+                                    <div>
+                                        <FormField
+                                            id={'resultAway'}
+                                            formData={this.state.formData.resultAway}
+                                            change={(element) => this.updateForm(element)}
+                                        />
+                                    </div>
+                                </div>
+                            </div>
                         </form>
                     </div>
                 </div>
