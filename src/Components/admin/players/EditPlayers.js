89f759147c2234c190e9220ba87f783581e3f65b
Nicholas Wease <skynet60@outlook.com>
2019-02-26 11:34:06 +0100

    Finished Player Admin started add players.

---
+++ src/Components/admin/players/EditPlayers.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class EditPlayers extends Component {
+    render() {
+        return (
+            <div>
+                EDIT HERE
+            </div>
+        );
+    }
+}
+
+export default EditPlayers;
--- src/Components/admin/players/index.js
+++ src/Components/admin/players/index.js
@@ -8,18 +8,81 @@ import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 import CircularProgress from '@material-ui/core/CircularProgress';
+import { firebasePlayers } from '../../../firebase';
+import {firebaseLoop, reverseArray} from "../../ui/miscellaneous";
 
 class AdminPlayers extends Component {
 
     state = {
-
+        isLoading: true,
+        players: []
     };
+
+    componentDidMount() {
+        firebasePlayers.once('value').then((snapshot) => {
+            const players = firebaseLoop(snapshot)
+
+            this.setState({
+                isLoading: false,
+                players: reverseArray(players)
+            })
+        })
+    }
 
     render() {
         return (
-            <div>
-                PLAYERS
-            </div>
+            <AdminLayout>
+                <div>
+                    <Paper>
+                        <Table>
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell>First Name</TableCell>
+                                    <TableCell>Last Name</TableCell>
+                                    <TableCell>Number</TableCell>
+                                    <TableCell>Position</TableCell>
+                                </TableRow>
+                            </TableHead>
+
+                            <TableBody>
+                                {this.state.players ? this.state.players.map((player, i) => (
+                                    <TableRow key={i}>
+                                        <TableCell>
+                                            <Link to={'/admin_players/add_players/${player.id}'}>
+                                                {player.name}
+                                            </Link>
+                                        </TableCell>
+
+                                        <TableCell>
+                                            <Link to={'/admin_players/add_players/${player.id}'}>
+                                                {player.lastname}
+                                            </Link>
+                                        </TableCell>
+
+                                        <TableCell>
+                                            <Link to={'/admin_players/add_players/${player.id}'}>
+                                                {player.number}
+                                            </Link>
+                                        </TableCell>
+
+                                        <TableCell>
+                                            <Link to={'/admin_players/add_players/${player.id}'}>
+                                                {player.position}
+                                            </Link>
+                                        </TableCell>
+                                    </TableRow>
+                                )) :null}
+                            </TableBody>
+                        </Table>
+                    </Paper>
+                    <div className='admin_progress'>
+                        { this.state.isLoading ?
+                            <CircularProgress thickness={7} style={{color:'#98c5e9'}} />
+                            :''
+                        }
+                    </div>
+                </div>
+            </AdminLayout>
         );
     }
 }
