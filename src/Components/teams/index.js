eeb50d83ad5bf6d0117c7ec0cda14b660f57b592
Nicholas Wease <skynet60@outlook.com>
2019-02-28 11:51:06 +0100

    worked on setting up promises for team route.

--- src/Components/teams/index.js
+++ src/Components/teams/index.js
@@ -1,7 +1,38 @@
 import React, {Component} from 'react';
-import PlayerCard from "../ui/PlayerCard";
+import PlayerCard from '../ui/PlayerCard';
+import Fade from 'react-reveal/Fade';
+import Stripes from '../../Resources/images/stripes.png';
+import { firebasePlayers, firebase } from '../../firebase';
+import { firebaseLoop } from '../ui/miscellaneous';
+import { Promise } from 'core-js';
 
 class Teams extends Component {
+
+    state = {
+        loading: true,
+        players: []
+    };
+
+    componentDidMount() {
+        firebasePlayers.once('value').then((snapshot) => {
+            const players = firebaseLoop(snapshot);
+            let promises = [];
+
+            for (let key in players){
+                promises.push(
+                    new Promise((resolve, reject) => {
+                        firebase.storage().ref('players')
+                            .child(players[key].image).getDownloadURL().then(url => {
+                                players[key].url = url;
+                                resolve()
+                        })
+                    })
+                )
+            }
+            Promise.all(promises)
+        })
+    }
+
     render() {
         return (
             <div>
