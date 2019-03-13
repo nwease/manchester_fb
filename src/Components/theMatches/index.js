ef79754c30306465adcc69418435ba75df8a7d30
Nicholas Wease <skynet60@outlook.com>
2019-03-13 11:40:54 +0100

    Minor changes.

--- src/Components/theMatches/index.js
+++ src/Components/theMatches/index.js
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-import CircularProgress from '@material-ui/core/CircularProgress';
 import { firebaseMatches } from '../../firebase';
 import { firebaseLoop, reverseArray } from '../ui/miscellaneous';
 import LeagueTable from './table';
